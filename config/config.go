3c95575f9bd6ddf77e4049dcda8ee30a8441af58
sjang <linux321>
2019-05-27 21:37:42 +0900

    test

--- config/config.go
+++ config/config.go
@@ -1,38 +1,38 @@
 package config
 
 import (
-  "os"
-  "fmt"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 type Config struct {
-	ServerPort           string `json:"serverPort,omitempty"`
-	RedisServer            []string `json:"redisServer,omitempty"`
-	WorkHomeDir          string `json:"workHomeDir,omitempty"`
-	TempDir              string `json:"tempDir,omitempty"`
-	LogConfig            logConfig `json:"logConfig,omitempty"`
-	BackupDir            string `json:"backupDir,omitempty"`
-	LogDir               string `json:"logDir,omitempty"`
-	DB             *DBConfig `json:"db,omitempty"`// for reservation
+	ServerPort  string    `json:"serverPort,omitempty"`
+	RedisServer []string  `json:"redisServer,omitempty"`
+	WorkHomeDir string    `json:"workHomeDir,omitempty"`
+	TempDir     string    `json:"tempDir,omitempty"`
+	LogConfig   logConfig `json:"logConfig,omitempty"`
+	BackupDir   string    `json:"backupDir,omitempty"`
+	LogDir      string    `json:"logDir,omitempty"`
+	DB          *DBConfig `json:"db,omitempty"` // for reservation
 }
 
 type logConfig struct {
-	LogDir               string `json:"logDir,omitempty"`
-	LogFilename          string `json:"logFilename,omitempty"`
-	LogFilenameFormat    string `json:"logFilenameFormat,omitempty"`
-	BackupDir            string `json:"backupDir,omitempty"`
+	LogDir            string `json:"logDir,omitempty"`
+	LogFilename       string `json:"logFilename,omitempty"`
+	LogFilenameFormat string `json:"logFilenameFormat,omitempty"`
+	BackupDir         string `json:"backupDir,omitempty"`
 }
 
 type DBConfig struct {
-	DBMSName  string `json:"dbmsName,omitempty"`
-	ServerHost string `json:"serverHost,omitempty"`
-	ServerPort string `json:"serverPort,omitempty"`
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
-	DBName string `json:"dbName,omitempty"`
-	MaxIdleConns int `json:"maxIdleConns,omitempty"`
-	MaxOpenConns int `json:"maxOpenConns,omitempty"`
+	DBMSName     string `json:"dbmsName,omitempty"`
+	ServerHost   string `json:"serverHost,omitempty"`
+	ServerPort   string `json:"serverPort,omitempty"`
+	Username     string `json:"username,omitempty"`
+	Password     string `json:"password,omitempty"`
+	DBName       string `json:"dbName,omitempty"`
+	MaxIdleConns int    `json:"maxIdleConns,omitempty"`
+	MaxOpenConns int    `json:"maxOpenConns,omitempty"`
 }
 
 var conf *Config
